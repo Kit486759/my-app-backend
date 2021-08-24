bae1f6a139a503cac0941ae4f645e526f01a79d9
kit486759 <kit486759@gmail.com>
2021-08-24 00:12:59 -0700

    basic auth

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const basicAuth = require('express-basic-auth')
+const cors= require('cors')
+
+app.use(cors())
+
+// app.use(basicAuth({
+//     users:{'admin':'admin'}
+// }))
+
+// use safe compare prevent time attack
+app.use(basicAuth({
+    authorizer: (userName, passWord) => {
+        const userMatch = basicAuth.safeCompare(userName, 'admin')
+        const passWordMatch = basicAuth.safeCompare(passWord, 'admin')
+        return userMatch & passWordMatch
+    }
+}))
+
+app.get('/', (req, res, next) => {
+    console.log('test')
+    res.end(`This is home page`)
+})
+
+app.use('/', (req, res, next) => {
+    // console.log(res.status)
+    res.status(404).end(`Error 404 - Page not found`)
+})
+
+app.listen(1111, err => {
+    if (err) return console.log(`There is an error${err}`)
+    console.log(`Server listening on port "${1111}"`)
+})
